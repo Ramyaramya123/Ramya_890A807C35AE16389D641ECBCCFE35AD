088637156c55fcce0a683ba33fd27c7e2986e25d
Ramyaramya123 <144669673+Ramyaramya123@users.noreply.github.com>
2023-09-11 14:23:25 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,7 @@
+n=int("enter year")
+if(n%400==0):                 
+  print("leap year")
+elif(n%100=0):                     
+  print("not a leap year")            elif(n%4==0):
+  printf("leap year")        
+else:                                 print("not a leap year")
